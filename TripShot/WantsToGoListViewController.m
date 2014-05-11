4665dcf8eca10ad4c82ebe311183ca01f7ec11db
EmikoFujiwara <esorange.door@gmail.com>
2014-05-12 04:34:44 +0900

    TSDataBaseクラス内一部変更、TableVC内一部追加

---
+++ TripShot/WantsToGoListViewController.m
@@ -0,0 +1,151 @@
+//
+//  WantsToGoListViewController.m
+//  TripShot
+//
+//  Created by bizan.com.mac05 on 2014/05/11.
+//  Copyright (c) 2014年 bizan.com.mac02. All rights reserved.
+//
+
+#import "WantsToGoListViewController.h"
+
+
+@interface WantsToGoListViewController ()
+@property NSMutableArray *idArray;
+@property NSMutableArray *titleArray;
+@property NSMutableArray *latArray;
+@property NSMutableArray *lotArray;
+@property NSMutableArray *addressArray;//緯度経度情報から住所を表示
+
+@end
+
+@implementation WantsToGoListViewController{
+}
+
+- (id)initWithStyle:(UITableViewStyle)style
+{
+    self = [super initWithStyle:style];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    // Uncomment the following line to preserve selection between presentations.
+    // self.clearsSelectionOnViewWillAppear = NO;
+    
+    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+
+    //使うデータをここで読み込む
+    TSDataBase *db = [[TSDataBase alloc]init];
+    
+//    [db loadData];
+
+    NSMutableArray *resultArray = [db loadDBData];
+    //ここにはそれぞれの配列がぼこっと入ってう。（deleteflagが0のやつね）
+    //必要な項目の配列をこの中から取り出すよ（中身もNSMutableArray）
+    //必要な項目？…id0、場所1、住所（つまり緯度2経度3）
+    
+    _idArray = [[NSMutableArray alloc]init];
+    _idArray = resultArray[0];
+
+    _titleArray = [[NSMutableArray alloc]init];
+    _titleArray = resultArray[1];
+
+    _latArray = [[NSMutableArray alloc]init];
+    _latArray = resultArray[2];
+    
+    _lotArray = [[NSMutableArray alloc]init];
+    _lotArray = resultArray[3];
+
+    //ここまでちゃんと通ってる。
+    NSLog(@"できるはずのセル個数%d",_idArray.count);
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    //リストのアイテム数が行になる
+    //ここではidが入っているものを一覧にしようか
+    return _idArray.count;
+}
+
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell *cell = [[UITableViewCell alloc]initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"Cell"];
+    
+//配列からアイテムを取得してLabelのテキストに入れる
+    cell.textLabel.text = _titleArray[indexPath.row];
+    cell.detailTextLabel.text = _addressArray[indexPath.row];
+    
+    return cell;
+}
+
+
+/*
+// Override to support conditional editing of the table view.
+- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    // Return NO if you do not want the specified item to be editable.
+    return YES;
+}
+*/
+
+/*
+// Override to support editing the table view.
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    if (editingStyle == UITableViewCellEditingStyleDelete) {
+        // Delete the row from the data source
+        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
+    } else if (editingStyle == UITableViewCellEditingStyleInsert) {
+        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+    }   
+}
+*/
+
+/*
+// Override to support rearranging the table view.
+- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath
+{
+}
+*/
+
+/*
+// Override to support conditional rearranging of the table view.
+- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    // Return NO if you do not want the item to be re-orderable.
+    return YES;
+}
+*/
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
---
+++ Weather.m
@@ -0,0 +1,78 @@
+//
+//  Weather.m
+//  TripShot
+//
+//  Created by EmikoFujiwara on 2014/05/11.
+//  Copyright (c) 2014年 bizan.com.mac02. All rights reserved.
+//
+
+#import "Weather.h"
+
+@implementation Weather{
+    CLLocationManager *locationManager;
+    double latitude;
+    double longitude;
+    
+}
+-(void)getLocation{
+
+    //現在の位置情報を取得
+    //まず位置情報通知の開始
+    if([CLLocationManager locationServicesEnabled]){
+       locationManager = [[CLLocationManager alloc]init];
+        NSLog(@"位置情報取得可能");
+    }else{
+        NSLog(@"位置情報は使用できません");
+    }
+    
+    //通知イベントをこのクラスで受け取る
+    locationManager.delegate = self;
+    
+    //最高精度を設定
+    locationManager.desiredAccuracy = kCLLocationAccuracyBestForNavigation;
+    
+    //移動距離が変化したときにイベントを起こす値（メートル）を指定、KCLDistanceFilerNoneで逐次
+    locationManager.distanceFilter = kCLDistanceFilterNone;
+    
+    //位置情報の取得を開始する
+    [locationManager startUpdatingLocation];
+    
+    NSLog(@"check0===========================");
+    
+}
+
+
+//受信した位置情報イベントの処理
+-(void)locationManager:(CLLocationManager *)manager didUpdateLocations:(NSArray *)locations{
+    
+    CLLocation *newLocation = [locations lastObject]; //これでnewLocationがとれてるらしい。
+    //    NSDate *eventDate = newLocation.timestamp;
+    NSLog(@"check1=============================latitude %+.6f, longitude %+.6f\n",
+          newLocation.coordinate.latitude,
+          newLocation.coordinate.longitude);
+    
+     latitude = newLocation.coordinate.latitude;
+     longitude = newLocation.coordinate.longitude;
+     
+    //位置情報の取得を停止する
+    [locationManager stopUpdatingLocation];
+
+}
+
+
+-(NSString *)getWeather{
+    //天気を取得
+    
+    //天気API
+    
+    //文字列に変換
+    
+    
+    
+    
+    NSString *weather;
+    return weather;
+}
+
+@end
+
