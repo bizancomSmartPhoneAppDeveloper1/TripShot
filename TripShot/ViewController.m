39cc1a23fd498a635885c8f9177a9dabf7ea9610
IkumaKomaya <bizan.com.mac03@apple-no-MacBook-Air-9.local>
2014-05-12 10:55:56 +0900

    とりあえず現在位置統合

--- TripShot/ViewController.m
+++ TripShot/ViewController.m
@@ -18,12 +18,24 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    self.locationManager = [[CLLocationManager alloc]init];
+    self.locationManager.delegate = self;
+    [self.locationManager startUpdatingLocation];
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+//位置情報が通知された時
+-(void)locationManager:(CLLocationManager *)manager didUpdateLocations:(NSArray *)locations
+{
+    //最新の位置情報を取り出す
+    CLLocation *location = [locations lastObject];
+    [self.mapView setCenterCoordinate:location.coordinate animated:YES];
 }
 
 @end
