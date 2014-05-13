dd5a58506191bfa2de9ee9d121c84471e2c797d1
EmikoFujiwara <esorange.door@gmail.com>
2014-05-13 11:52:25 +0900

    マージに失敗とりあえずcommit

---
+++ TripShot/SearchListViewController.h
@@ -0,0 +1,13 @@
+//
+//  SearchListViewController.h
+//  TripShot
+//
+//  Created by bizan.com.mac05 on 2014/05/13.
+//  Copyright (c) 2014年 bizan.com.mac02. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SearchListViewController : UITableViewController
+
+@end
