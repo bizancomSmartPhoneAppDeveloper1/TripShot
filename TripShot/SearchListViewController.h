11c6ba57da0adda6e1686634b5ee2a40d1c30bc8
EmikoFujiwara <esorange.door@gmail.com>
2014-05-14 17:57:31 +0900

    pullするまえのcommit

--- TripShot/SearchListViewController.h
+++ TripShot/SearchListViewController.h
@@ -2,13 +2,13 @@
 //  SearchListViewController.h
 //  TripShot
 //
-//  Created by bizan.com.mac05 on 2014/05/13.
+//  Created by EmikoFujiwara on 2014/05/13.
 //  Copyright (c) 2014年 bizan.com.mac02. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 
-@interface SearchListViewController : UITableViewController
+@interface SearchListViewController : UITableViewController<UISearchDisplayDelegate,UISearchBarDelegate>
 @property (weak, nonatomic) IBOutlet UISearchBar *searchField;
 - (IBAction)cancelButtonTapped:(id)sender;
 - (IBAction)addButtonTapped:(id)sender;
--- TripShot/SearchListViewController.m
+++ TripShot/SearchListViewController.m
@@ -2,18 +2,25 @@
 //  SearchListViewController.m
 //  TripShot
 //
-//  Created by bizan.com.mac05 on 2014/05/13.
+//  Created by EmikoFujiwara on 2014/05/13.
 //  Copyright (c) 2014年 bizan.com.mac02. All rights reserved.
 //
 
 #import "SearchListViewController.h"
 #import "TSDataBase.h"
 
-@interface SearchListViewController ()
+@interface SearchListViewController (){
+
+    NSMutableArray *resultArray;
+
+}
 
 @end
 
 @implementation SearchListViewController
+
+NSString * const APIKEY = @"dj0zaiZpPXpXNGNjRWtiNG83ViZzPWNvbnN1bWVyc2VjcmV0Jng9MmM-";
+
 
 - (id)initWithStyle:(UITableViewStyle)style
 {
@@ -33,6 +40,57 @@
     
     // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
     // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+    
+    NSMutableArray *resultArray = [[NSMutableArray alloc]init]; //tableビューに表示するための配列準備
+    
+    //入力された文字列を一旦格納
+    NSString *searchword = @"ハタダ";//_searchField.text; //入力された文字列を一旦格納
+    // UTF-8でエンコード
+    NSString *encodedString = [searchword stringByAddingPercentEscapesUsingEncoding:
+                               NSUTF8StringEncoding];
+
+    NSString *path = [NSString stringWithFormat:@"http://search.olp.yahooapis.jp/OpenLocalPlatform/V1/localSearch?appid=%@&query=%@",APIKEY,encodedString];
+    
+    
+    NSURL *url = [NSURL URLWithString:path];
+    NSURLRequest *request = [NSURLRequest requestWithURL:url];
+    //WebAPIからNSData形式でJSONデータを取得
+    NSData *jsonData = [NSURLConnection sendSynchronousRequest:request returningResponse:nil error:nil];
+    
+    //よくみてねここから
+    if(jsonData){
+        
+        NSError *jsonParsingError = nil;
+        //JSONからNSDictionaryまたはNSArrayに変換
+        //JSONによって、配列ならばNSArrayになりそうでなければNSDictionaryとなる
+        NSDictionary *dic = [NSJSONSerialization JSONObjectWithData:jsonData options:NSJSONReadingAllowFragments error:&jsonParsingError];
+        
+        NSLog(@"%@",dic);
+        
+//        //NSDictionaryを利用して、必要なデータを取得する
+//        NSString *status = [dic objectForKey:@"status"];
+//        NSLog(@"status: %@",status);
+//        
+//        NSArray *arrayResult = [dic objectForKey:@"results"];
+//        NSDictionary *resultDic = [arrayResult objectAtIndex:0];
+//        NSDictionary *geometryDic = [resultDic objectForKey:@"geometry"];
+//        NSLog(@"geometryDic : %@",geometryDic);
+//        
+//        NSDictionary *locationDic = [geometryDic objectForKey:@"location"];
+//        NSNumber *lat = [locationDic objectForKey:@"lat"];
+//        NSNumber *lng = [locationDic objectForKey:@"lng"];
+//        NSLog(@"location lat = %@, lng = %@",lat,lng);
+        
+    }else{
+        
+        NSLog(@"the connection could not be created or if the download fails.");
+        
+    }
+
+
+    //ここまで　解析要
+    
+    
 }
 
 - (void)didReceiveMemoryWarning
